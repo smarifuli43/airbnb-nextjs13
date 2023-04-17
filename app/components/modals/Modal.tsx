7dfdaa1e485ee95b55440ae5d45d4b7785acfdcb
smarifuli43 <smarifulislami43@gmail.com>
2023-04-18 04:07:30 +0600

    navbar

---
+++ app/components/modals/Modal.tsx
@@ -0,0 +1,33 @@
+'use client';
+import React, { useState } from 'react';
+
+interface ModalProps {
+  isOpen?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  title?: string;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
+  actionLabel: string;
+  disabled?: boolean;
+  secondaryAction?: () => void;
+  secondaryActionLabel?: string;
+}
+
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  onClose,
+  onSubmit,
+  title,
+  body,
+  actionLabel,
+  footer,
+  disabled,
+  secondaryAction,
+  secondaryActionLabel,
+}) => {
+  const [showModal, setShowModal] = useState(isOpen);
+  return <div></div>;
+};
+
+export default Modal;
