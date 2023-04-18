6f34cab1d6538c970b0bd5bb0c6c3ce966880a89
smarifuli43 <smarifulislami43@gmail.com>
2023-04-19 03:49:43 +0600

    modal

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -3,8 +3,10 @@ import React, { useCallback, useState } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
 import Avatar from '../Avatar';
 import MenuItem from './MenuItem';
+import useRegisterModal from '@/app/hooks/useRegisterModal';
 
 const UserMenu = () => {
+  const registerModal = useRegisterModal();
   const [isOpen, setIsOpen] = useState(false);
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
@@ -71,7 +73,7 @@ const UserMenu = () => {
           <div className='flex flex-col cursor-pointer'>
             <></>
             <MenuItem label='login' onClick={() => {}} />
-            <MenuItem label='Sign up' onClick={() => {}} />
+            <MenuItem label='Sign up' onClick={registerModal.onOpen} />
           </div>
         </div>
       )}
