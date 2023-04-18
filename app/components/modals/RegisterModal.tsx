3bf1ab48f44cbdce6c5915d6a56b5c7939cf3e8c
smarifuli43 <smarifulislami43@gmail.com>
2023-04-19 04:11:11 +0600

    register modal

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -10,6 +10,10 @@ import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal';
+import Heading from '../Heading';
+import Input from '../inputs/Input';
+import Button from '../Button';
+import { toast } from 'react-hot-toast';
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -35,12 +39,85 @@ const RegisterModal = () => {
         registerModal.onClose();
       })
       .catch((err) => {
-        console.log(err);
+        toast.error('Something went wrong');
       })
       .finally(() => {
         setIsLoading(false);
       });
   };
+
+  const bodyContent = (
+    <div className='flex flex-col gap-4'>
+      <Heading title='Welcome to Airbnb' subtitle='Create an account!' />
+      <Input
+        id='email'
+        label='Email'
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id='name'
+        label='Name'
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id='password'
+        label='Password'
+        type='password'
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  );
+
+  const footerContent = (
+    <div className='flex flex-col gap-4 mt-3'>
+      <hr />
+      <Button
+        outline
+        label='Continue with Google'
+        icon={FcGoogle}
+        // onClick={() => signIn('google')}
+      />
+      <Button
+        outline
+        label='Continue with Github'
+        icon={AiFillGithub}
+        // onClick={() => signIn('github')}
+      />
+      <div
+        className='
+          text-neutral-500 
+          text-center 
+          mt-4 
+          font-light
+        '
+      >
+        <p>
+          Already have an account?
+          <span
+            onClick={registerModal.onClose}
+            className='
+              text-neutral-800
+              cursor-pointer 
+              hover:underline
+            '
+          >
+            {' '}
+            Log in
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+
   return (
     <Modal
       disabled={isLoading}
@@ -49,8 +126,8 @@ const RegisterModal = () => {
       actionLabel='Continue'
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
-      // body={bodyContent}
-      // footer={footerContent}
+      body={bodyContent}
+      footer={footerContent}
     />
   );
 };
